1f9bc269ef7b919f9a5d1eab3a0c8d18818cdfb9
chloelczx <chloelancezeux@gmail.com>
2025-12-11 18:03:08 +0100

    Ajout dynamique des projets à la galerie

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,6 +2,23 @@
 async function getWorks() {
     const worksResponse = await fetch("http://localhost:5678/api/works");
     const works = await worksResponse.json();
+
+    // Affichage des projets dans la gallerie
+    const gallery = document.querySelector(".gallery");
+
+    works.forEach(work => {
+        const workContent = document.createElement("figure");
+        const workImg = document.createElement("img");
+        const workCaption = document.createElement("figcaption");
+
+        workImg.src = work.imageUrl;
+        workImg.alt = work.title;
+        workCaption.textContent = work.title;
+
+        workContent.appendChild(workImg);
+        workContent.appendChild(workCaption);
+        gallery.appendChild(workContent);
+    });
 }
 
 getWorks();
