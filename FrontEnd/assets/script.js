d21aa976060ce8a200dde23e3fe3105a5c2409f5
chloelczx <chloelancezeux@gmail.com>
2025-12-15 18:21:48 +0100

    Ajout dynamique des filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,14 +1,15 @@
+const gallery = document.querySelector(".gallery");
+
+
 // Appel API pour récupérer les projets de l'architecte
 async function getWorks() {
     const worksResponse = await fetch("http://localhost:5678/api/works");
     const works = await worksResponse.json();
 
-    // Affichage des projets dans la gallerie
-    const gallery = document.querySelector(".gallery");
-
     // Suppression contenu HTML des projets
     gallery.innerHTML = "";
 
+    // Affichage des projets dans la gallerie
     works.forEach(work => {
         const workContent = document.createElement("figure");
         const workImg = document.createElement("img");
@@ -30,4 +31,21 @@ getWorks();
 async function getCategories() {
     const categoriesResponse = await fetch("http://localhost:5678/api/categories");
     const categories = await categoriesResponse.json();
+
+    // Affichage des filtres
+    const portfolio = document.getElementById("portfolio");
+    const filters = document.createElement("div");
+    filters.classList.add("filters");
+
+    categories.forEach(category => {
+        const btnFilter = document.createElement("button");
+        btnFilter.classList.add("filter");
+        btnFilter.textContent = category.name;
+
+        filters.appendChild(btnFilter);
+    });
+
+    portfolio.insertBefore(filters, gallery);
 }
+
+getCategories();
