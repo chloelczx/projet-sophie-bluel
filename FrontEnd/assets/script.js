daf273f4398fa647eeb1433eb3dd6afedd7f7440
chloelczx <chloelancezeux@gmail.com>
2025-12-16 10:48:10 +0100

    Modifications fonctionnalité filtres : variable de stockage des travaux rendue globale + création fonction d'affichage des travaux

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,28 +1,32 @@
 const gallery = document.querySelector(".gallery");
-
+let allWorks = [];
 
 // Appel API pour récupérer les projets de l'architecte
 async function getWorks() {
     const worksResponse = await fetch("http://localhost:5678/api/works");
-    const works = await worksResponse.json();
-
-    // Suppression contenu HTML des projets
-    gallery.innerHTML = "";
+    allWorks = await worksResponse.json();
 
     // Affichage des projets dans la gallerie
-    works.forEach(work => {
-        const workContent = document.createElement("figure");
-        const workImg = document.createElement("img");
-        const workCaption = document.createElement("figcaption");
+    function displayWorks(works) {
+        // Suppression contenu HTML des projets
+        gallery.innerHTML = "";
 
-        workImg.src = work.imageUrl;
-        workImg.alt = work.title;
-        workCaption.textContent = work.title;
+        works.forEach(work => {
+            const workContent = document.createElement("figure");
+            const workImg = document.createElement("img");
+            const workCaption = document.createElement("figcaption");
 
-        workContent.appendChild(workImg);
-        workContent.appendChild(workCaption);
-        gallery.appendChild(workContent);
-    });
+            workImg.src = work.imageUrl;
+            workImg.alt = work.title;
+            workCaption.textContent = work.title;
+
+            workContent.appendChild(workImg);
+            workContent.appendChild(workCaption);
+            gallery.appendChild(workContent);
+        });
+    }
+
+    displayWorks(allWorks);
 }
 
 getWorks();
