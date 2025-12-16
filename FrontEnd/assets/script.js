a9052f8ed5d74a3d64973ab453ccefcd3ebeabfa
chloelczx <chloelancezeux@gmail.com>
2025-12-16 13:56:40 +0100

    Modification structurelle fonctionnalité filtres : fonction d'affichage des travaux rendue accessible

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,30 +1,30 @@
 const gallery = document.querySelector(".gallery");
 let allWorks = [];
 
+// Fonction d'affichage des projets dans la gallerie
+function displayWorks(works) {
+    // Suppression contenu HTML des projets
+    gallery.innerHTML = "";
+
+    works.forEach(work => {
+        const workContent = document.createElement("figure");
+        const workImg = document.createElement("img");
+        const workCaption = document.createElement("figcaption");
+
+        workImg.src = work.imageUrl;
+        workImg.alt = work.title;
+        workCaption.textContent = work.title;
+
+        workContent.appendChild(workImg);
+        workContent.appendChild(workCaption);
+        gallery.appendChild(workContent);
+    });
+}
+
 // Appel API pour récupérer les projets de l'architecte
 async function getWorks() {
     const worksResponse = await fetch("http://localhost:5678/api/works");
     allWorks = await worksResponse.json();
-
-    // Affichage des projets dans la gallerie
-    function displayWorks(works) {
-        // Suppression contenu HTML des projets
-        gallery.innerHTML = "";
-
-        works.forEach(work => {
-            const workContent = document.createElement("figure");
-            const workImg = document.createElement("img");
-            const workCaption = document.createElement("figcaption");
-
-            workImg.src = work.imageUrl;
-            workImg.alt = work.title;
-            workCaption.textContent = work.title;
-
-            workContent.appendChild(workImg);
-            workContent.appendChild(workCaption);
-            gallery.appendChild(workContent);
-        });
-    }
 
     displayWorks(allWorks);
 }
