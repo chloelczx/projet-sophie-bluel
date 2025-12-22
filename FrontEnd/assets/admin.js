a6b1e2e66f236540735237319629c25cc1370d4c
chloelczx <chloelancezeux@gmail.com>
2025-12-22 17:36:39 +0100

    Requête POST : envoi valeurs des entrées du formulaire à l'API

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -2,9 +2,18 @@ const loginForm = document.querySelector("form");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
-loginForm.addEventListener("submit", event => {
+loginForm.addEventListener("submit", async (event) => {
+    // Bloque rechargement de la page par défaut de l'input type submit
     event.preventDefault();
 
     const email = emailInput.value;
     const password = passwordInput.value;
+
+    const loginResponse = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            Headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ email, password })
+        });
+
+    const loginData = await loginResponse.json();
 });
