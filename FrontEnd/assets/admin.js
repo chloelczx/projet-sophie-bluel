2819d685d5610a88f08203f503b7cfd4bd54768b
chloelczx <chloelancezeux@gmail.com>
2025-12-22 14:15:23 +0100

    Récupération données formulaire

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -1,5 +1,10 @@
 const loginForm = document.querySelector("form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 
 loginForm.addEventListener("submit", event => {
     event.preventDefault();
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
 });
