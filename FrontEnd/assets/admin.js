f7e9fdd08420f82444e04e305deda92f5f5022fa
chloelczx <chloelancezeux@gmail.com>
2025-12-22 18:52:36 +0100

    Intégration de la gestion de réponse de l'API

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -2,18 +2,36 @@ const loginForm = document.querySelector("form");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
+// Création du message d'erreur
+const errorMessage = document.createElement("p");
+errorMessage.classList.add("login-error");
+errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
+
+
+
+// Ecoute de l'événement : envoi du formulaire
 loginForm.addEventListener("submit", async (event) => {
-    // Bloque rechargement de la page par défaut de l'input type submit
+    // Bloque le rechargement de la page par défaut de l'input type submit
     event.preventDefault();
 
+    // Requête POST à l'API : envoi des valeurs du formulaire
     const email = emailInput.value;
     const password = passwordInput.value;
 
     const loginResponse = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
-            Headers: { "Content-type": "application/json" },
+            headers: { "Content-type": "application/json" },
             body: JSON.stringify({ email, password })
         });
-
     const loginData = await loginResponse.json();
+
+    // Gestion de la réponse de l'API
+    if (loginResponse.ok) {
+        localStorage.setItem("token", loginData.token);
+        window.location.href = "index.html";
+    } else {
+        if (!loginForm.contains(errorMessage)) {
+            loginForm.appendChild(errorMessage);
+        }
+    }
 });
