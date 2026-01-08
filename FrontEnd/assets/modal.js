871068f0d878cc7fec6c4546ff5ef7991e5eec38
chloelczx <chloelancezeux@gmail.com>
2026-01-08 21:19:41 +0100

    Intégration fonction de fermeture de la modale

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -5,9 +5,10 @@ const modal = document.getElementById("modal");
 
 // Fonction d'affichage des travaux dans la galerie de la modale
 function displayWorksInModal (allWorks) {
+    const modalWorksContainer = document.querySelector(".modal-works");
+    modalWorksContainer.innerHTML = "";
+    
     allWorks.forEach(work => {
-        const modalWorksContainer = document.querySelector(".modal-works");
-        
         const modalWorkContent = document.createElement("figure");
         const modalWorkImg = document.createElement("img");
         modalWorkImg.src = work.imageUrl;
@@ -28,7 +29,7 @@ function displayWorksInModal (allWorks) {
 
 
 
-// Fonction d'affichage modale
+// Fonction d'affichage de la modale
 export function openModal (allWorks) {
     modal.classList.add("active");
     modal.setAttribute("role", "dialog");
@@ -38,3 +39,24 @@ export function openModal (allWorks) {
     // Appel fonction d'affichage des travaux
     displayWorksInModal(allWorks);
 }
+
+
+
+// Fonction de fermeture de la modale
+function closeModal () {
+    modal.classList.remove("active");
+    modal.removeAttribute("role", "dialog");
+    modal.removeAttribute("aria-modal", "true");
+    modal.setAttribute("aria-hidden", "true");
+}
+
+// Appel fonction de fermeture de la modale au clic sur la croix
+const closeModalBtn = document.querySelector(".close-modal");
+closeModalBtn.addEventListener("click", closeModal);
+
+// Appel fonction de fermeture au clic en dehors de la modale
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
